96d15ec5732f45e41674bbb16d2415cdc7de038a
samuelbediako <samuelbediako0245@gmail.com>
2025-03-02 05:18:04 -1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Components/Home/home";
+import './App.css'
+
+function App() {
+
+  return (
+    <Home>
+      <Home />
+    </Home>
+  )
+}
+
+export default App
---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,585 @@
+import React from "react";
+import "./home.css";
+import "../../../public/assets/fontawesome/css/all.css";
+
+function Home() {
+  return (
+    <>
+      <div className="main-container">
+        <nav className="navbar-big">
+          <div className="log-pagn">
+            <div className="logo">
+              <h1 className="fas fa-play"></h1>
+              <h1 className="logname">Vercel</h1>
+            </div>
+
+            <ul className="pagesnav">
+              <li className="prodnav">
+                <a href="#">
+                  Product <i class="fas fa-chevron-down"></i>
+                </a>
+
+                <div className="prd-nn">
+                  <div className="prod-dpl">
+                    <div className="prod-dpl1">
+                      <h3>DX Platform</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="prod-dpl1">
+                      <h3>Manage infarastructure</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="prod-dpl1">
+                      <h3>Open Source</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li className="solunav">
+                <a href="#">
+                  Solutions <i class="fas fa-chevron-down"></i>
+                </a>
+
+                <div className="prd-nn2">
+                  <div className="prod-dpl">
+                    <div className="prod-dpl1">
+                      <h3>Use Cases</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="prod-dpl1">
+                      <h3>Users</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li className="reso">
+                <a href="#">
+                  Resources <i class="fas fa-chevron-down"></i>
+                </a>
+
+                <div className="prd-nn3">
+                  <div className="prod-dpl">
+                    <div className="prod-dpl1">
+                      <h3>Tools</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="prod-dpl1">
+                      <h3>Company</h3>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                      <br />
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+
+                      <br />
+                      <br />
+
+                      <div className="compnet">
+                        <i className="fas fa-car"></i>
+
+                        <div className="cp-txt">
+                          <h3>Preview</h3>
+                          <p>Helpingteams ship 6x faster</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <a href="#">Enterprise</a>
+              </li>
+              <li>
+                <a href="#">Docs</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="add-info">
+            <a href="#">Log In</a>
+            <a href="#">Contact</a>
+            <a
+              style={{
+                background: "#fff",
+                color: "black",
+              }}
+              href="#"
+            >
+              Sign Up
+            </a>
+          </div>
+        </nav>
+
+        <div className="grd-div">
+          <div className="grdimg">
+            <img className="gimgg" src="src/assets/gridimg.png" alt="" />
+            <div className="grtext">
+              <h1>Your complete Platform for the web</h1>
+              <p>
+                Vercel provides the developer tools and cloud infrastructure to
+                build, scale, and secure a faster, more personalized web..
+              </p>
+
+              <div className="st-gd-btn-div">
+                <a href="#">Start Deploying</a>
+                <a
+                  style={{
+                    backgroundColor: "black",
+                    color: "#fff",
+                  }}
+                  href="#"
+                >
+                  Get a Demo
+                </a>
+              </div>
+
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <div className="prmd-div">
+                <img src="src/assets/golden.png" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="rect">
+            <h1>
+              Develop with your favorite tools{" "}
+              <span className="fas fa-greater-than"></span>_
+            </h1>
+            <br />
+            <h1>
+              {" "}
+              Launch globally, instantly <span className="fas fa-globe"></span>{" "}
+              Keep pushing{" "}
+            </h1>
+          </div>
+
+          <div className="spce"></div>
+
+          <div className="gitt">
+            <div className="lft">
+              <h3>
+                <span class="fas fa-greater-than"></span>_Git-connected Deploys
+              </h3>
+              <br />
+              <h2>
+                From localhost to https, in seconds. Deploy from Git or your
+                CLI.
+              </h2>
+              <br />
+              <br />
+              <div className="glimg">
+                <img
+                  style={{
+                    objectFit: "contain",
+                  }}
+                  src="src\assets\Screenshot 2025-03-02 040454.png"
+                  alt="sh"
+                />
+              </div>
+            </div>
+
+            <div className="rit">
+              <h3>
+                <span class="fas fa-message"> </span> Collaborative
+                pre-production
+              </h3>
+              <br />
+              <h2>
+                Every deploy is remarkable. Chat with your team on real,
+                production-grade UI, not just designs.
+              </h2>
+              <br />
+              <br />
+              <div className="grimg">
+                <img src="src\assets\chat.png" alt="sh" />
+              </div>
+            </div>
+          </div>
+
+          <div className="stats">
+            <h3>
+              <span class="fas fa-message"> </span> Frontend Observability
+            </h3>
+            <br />
+            <h2>
+              Every deploy is remarkable. Chat with your team on real,
+              production-grade UI, not just designs.
+            </h2>
+          </div>
+
+          <div className="gitt">
+            <div className="lft">
+              <h3>
+                <span class="fas fa-greater-than"></span>_Instant Rollbacks
+              </h3>
+              <br />
+              <h2>
+                Go ahead, deploy on Friday. Instantly rollback to a working
+                deployment. CLI.
+              </h2>
+              <br />
+              <br />
+              <div className="glimg">
+                <img
+                  style={{
+                    objectFit: "contain",
+                  }}
+                  src="src\assets\l1.png"
+                  alt="sh"
+                />
+              </div>
+            </div>
+
+            <div className="rit">
+              <h3>
+                <span class="fas fa-message"> </span> Conformance
+              </h3>
+              <br />
+              <h2>
+                Move fast, don’t break things. Keep quality high while
+                maintaining velocity with Enterprise Monorepos.
+              </h2>
+              <br />
+              <br />
+              <div className="grimg">
+                <img src="src\assets\l2.png" alt="sh" />
+              </div>
+            </div>
+          </div>
+
+          <div className="scaleb">
+            <h2>
+              {" "}
+              Scale{" "}
+              <span>
+                <i class="fa-solid fa-building"></i> Enterprise
+              </span>{" "}
+              your without compromising{" "}
+              <span>
+                <i class="fa-solid fa-shield"></i> Security
+              </span>{" "}
+            </h2>
+          </div>
+          <div className="spce"></div>
+
+          <div className="readyy">
+            <div className="llft">
+              <h3>
+                Ready to deploy? Start building with a free account. Speak to an
+                expert for your{" "}
+                <sspan
+                  style={{
+                    backgroundColor: "black",
+                    color: "cyan",
+                  }}
+                >
+                  Pro{" "}
+                </sspan>
+                or{" "}
+                <sspan
+                  style={{
+                    backgroundColor: "black",
+                    color: "purple",
+                  }}
+                >
+                  Enterprise{" "}
+                </sspan>{" "}
+                needs.
+              </h3>
+
+              <div className="st-gd-btn-divv">
+                <a href="#">Start Deploying</a>
+                <a
+                  style={{
+                    backgroundColor: "black",
+                    color: "#fff",
+                  }}
+                  href="#"
+                >
+                  Talk to an Expert
+                </a>
+              </div>
+            </div>
+
+            <div className="rrft">
+              <h3>
+                Ready to deploy? Start building with a free account. Speak to an
+                expert for your Pro or Enterprise needs.
+              </h3>
+
+              <div className="st-gd-btn-divv">
+                <a
+                  style={{
+                    backgroundColor: "black",
+                    color: "#fff",
+                  }}
+                  href="#"
+                >
+                  Explore Enterprise
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
